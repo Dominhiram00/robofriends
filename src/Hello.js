867b21fe7496a7c9af968b3ef6eea61d2c27b958
Hiram Dominguez <dominhiram01@gmail.com>
2023-03-01 15:16:33 -0600

    3/1/2023

---
+++ src/Hello.js
@@ -0,0 +1,7 @@
+import React, {Component} from "react"; //{} doesn't require us to use React.Component
+
+class Hello extends Component{
+    render(){
+        return <h1>Hello World</h1>
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; //glue of React for desktop web apps
 import './index.css';
-import App from './App';
+import Hello from './Hello.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1> Hello World</h1>
     <App />
-  </React.StrictMode>
+  </React.StrictMode> 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
