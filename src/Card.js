2e025e499bcf2c40ad3f3151dc28256269567817
Hiram Dominguez <dominhiram01@gmail.com>
2023-03-01 17:41:38 -0600

    Robotfriends start

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () =>{
+    return(
+        <div>
+            <img alt='robots' src=''/>
+            <div>
+                <h2>Hamill Heck</h2>
+                <p>hamillheck@urdad.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; //glue of React for desktop web apps
 import './index.css';
-import Hello from './Hello.js';
+import Card from './Card.js';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello greeting = {'Hello' + 'React Wizard'}> </Hello>
+    <Card> </Card>
   </React.StrictMode> 
 );
 
