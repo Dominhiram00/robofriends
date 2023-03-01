c6164d3987f141321f2b02a8ccd0a2146a672dd2
Hiram Dominguez <dominhiram01@gmail.com>
2023-03-01 15:31:22 -0600

    tachyons installed

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,15 @@
 import React, {Component} from "react"; //{} doesn't require us to use React.Component
+import './Hello.css';
 
 class Hello extends Component{
     render(){
-        return <h1>Hello World</h1>
+        return (
+        <div>
+        <h1>Hello World</h1>
+        <p> Welcome to the cumzone</p>
+        </div>
+        )
     }
 }
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'; //glue of React for desktop web apps
 import './index.css';
 import Hello from './Hello.js';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1> Hello World</h1>
-    <App />
+    <Hello></Hello>
   </React.StrictMode> 
 );
 
