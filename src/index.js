1aa5a5fe8a483e3b6c0de948a4d5d92e426f41e8
Hiram Dominguez <dominhiram01@gmail.com>
2023-03-01 21:36:34 -0600

    Key = x for loops

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; //glue of React for desktop web apps
 import './index.css';
-import Card from './Card.js';
+//import Card from './Card.js';
+import CardList from './CardList'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { robots } from './Robots'; //Non-default js components must be destruct
@@ -9,9 +10,7 @@ import { robots } from './Robots'; //Non-default js components must be destruct
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <Card id = {robots[0].id} name ={robots[0].name} email={robots[0].email}> </Card>
-    <Card id = {robots[1].id} name ={robots[1].name} email={robots[1].email}> </Card>
-    <Card id = {robots[2].id} name ={robots[2].name} email={robots[2].email}> </Card>s
+    <CardList robots = {robots}/>
   </div>
     
 );
